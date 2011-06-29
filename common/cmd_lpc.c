90a40a7a8671054fd0f4217738739b5b81217b46
Matt Isaacs <izzy@buglabs.net>
2011-06-29 15:45:31 -0400

    LPC IO and Memory transfer fixes.
    Extended SYNC wait times to 20 clock cycles.
    Added lpc_init to write cycles. this performs resets, and ensures all GPIO directions are correctly set.
    
    Signed-off-by: Matt Isaacs <izzy@buglabs.net>

--- common/cmd_lpc.c
+++ common/cmd_lpc.c
@@ -27,6 +27,7 @@
 
 void pulse_clk_out(int delay)
 {
+	/* delay = delay * 100000; */
 	udelay(delay);
 	lpc_set_clk(0);
 	udelay(delay);
@@ -35,6 +36,7 @@ void pulse_clk_out(int delay)
 
 void pulse_clk_in(int delay)
 {
+	/* delay = delay * 100000; */
 	udelay(delay);
 	lpc_set_clk(1);
 	udelay(delay);
@@ -89,12 +91,13 @@ unsigned char lpc_mem_read(unsigned int addr)
 
 	udelay(2);
 	lpc_set_clk(0);
-	while (lpc_get_lad() != LPC_SYNC_READY || (count < 6)) {
+	while (lpc_get_lad() != LPC_SYNC_READY || (count < 20)) {
 		pulse_clk_in(2);
 		count++;
 	}
-	return 0;
 
+	if (count >= 20)
+		printf("%s: No Sync detected.\n", __FUNCTION__);
 	/* Data */
 	pulse_clk_in(2);
 	data = lpc_get_lad();
@@ -110,6 +113,9 @@ unsigned char lpc_mem_read(unsigned int addr)
 
 int lpc_mem_write(unsigned int addr, unsigned char data)
 {
+	int count = 0;
+
+	lpc_init();
 	/* START */
 	lpc_set_clk(1);
 	lpc_set_lframe(0);
@@ -156,9 +162,13 @@ int lpc_mem_write(unsigned int addr, unsigned char data)
 
 	udelay(2);
 	lpc_set_clk(0);
-	while (lpc_get_lad() != LPC_SYNC_READY)
+	while ((lpc_get_lad() != LPC_SYNC_READY) || (count < 20)) {
 		pulse_clk_in(2);
-	return 0;
+		count++;
+	}
+
+	if (count >= 20)
+		printf("%s: No Sync detected.\n", __FUNCTION__);
 
 	/* TAR */
 	pulse_clk_in(2);
@@ -207,11 +217,13 @@ unsigned char lpc_io_read(unsigned short addr)
 
 	udelay(2);
 	lpc_set_clk(0);
-	while (lpc_get_lad() != LPC_SYNC_READY || (count < 6)) {
+	while (lpc_get_lad() != LPC_SYNC_READY || (count < 20)) {
 		pulse_clk_in(2);
 		count++;
 	}
-	return 0;
+
+	if (count >= 20)
+		printf("%s: No Sync detected.\n", __FUNCTION__);
 
 	/* Data */
 	pulse_clk_in(2);
@@ -228,6 +240,9 @@ unsigned char lpc_io_read(unsigned short addr)
 
 int lpc_io_write(unsigned short addr, unsigned char data)
 {
+	int count = 0;
+
+	lpc_init();
 	/* START */
 	lpc_set_clk(1);
 	lpc_set_lframe(0);
@@ -266,9 +281,13 @@ int lpc_io_write(unsigned short addr, unsigned char data)
 
 	udelay(2);
 	lpc_set_clk(0);
-	while (lpc_get_lad() != LPC_SYNC_READY)
+	while ((lpc_get_lad() != LPC_SYNC_READY) || (count < 20)) {
 		pulse_clk_in(2);
-	return 0;
+		count++;
+	}
+
+	if (count >= 20)
+		printf("%s: No Sync detected.\n", __FUNCTION__);
 
 	/* TAR */
 	pulse_clk_in(2);
@@ -283,7 +302,7 @@ int do_lpc (cmd_tbl_t *cmdtp, int flag, int argc, char* const argv[])
 	unsigned short io_addr;
 	unsigned char data;
 
-	printf("argc: %d\n", argc);
+	/* printf("argc: %d\n", argc); */
 
 	if (argc < 2)
 		return cmd_usage(cmdtp);
@@ -319,7 +338,7 @@ int do_lpc (cmd_tbl_t *cmdtp, int flag, int argc, char* const argv[])
 			io_addr = (unsigned short)simple_strtoul(argv[2], NULL, 16);
 			data = (unsigned char)(0xf & simple_strtoul(argv[3], NULL, 16));
 			data = lpc_io_write(io_addr, data);
-			printf("LPC mem_write: 0x%x\n",data);
+			printf("LPC io_write: 0x%x\n",data);
 			return 0;
 		}
 		else
@@ -331,4 +350,6 @@ U_BOOT_CMD(
 	lpc, 4, 0, do_lpc,
 	"LPC Subsystem",
 	"mem_read addr\n"
-	"lpc mem_write addr data\n");
+	"lpc mem_write addr data\n"
+	"lpc io_read addr\n"
+	"lpc io_write addr data\n");
